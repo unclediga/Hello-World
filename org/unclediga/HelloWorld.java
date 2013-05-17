44dbdb0a63c4c2c5653382f724266308cb60cf0a
unclediga <zowie_i@mail.ru>
2013-05-17 11:36:41 +0400

    insertion java

--- org/unclediga/HelloWorld.java
+++ org/unclediga/HelloWorld.java
@@ -3,5 +3,6 @@ package org.unclediga;
 public class HelloWorld {
   public static void main(String[] args){
       System.out.println("Hello, world!");
+      System.out.println("One more time:Hello, world!");
   }  	
 }
